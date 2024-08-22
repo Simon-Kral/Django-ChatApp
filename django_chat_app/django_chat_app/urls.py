af159b6d974551aa5515b337945dcbb014a754db
Simon Kral <simonkral92@gmail.com>
2024-08-22 21:54:37 +0200

    routed chat app

--- django_chat_app/django_chat_app/urls.py
+++ django_chat_app/django_chat_app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for first_django_app project.
+URL configuration for django_chat_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from chat.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('chat/', index),
 ]
